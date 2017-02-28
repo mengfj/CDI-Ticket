4ab39f0fd02d90b662dc6e7b63a7fa888c7a4cc7
id_rsa git@github.ibm.com:chaser/BlueInsightService.git <meg@10.20.14.167>
2017-02-28 09:26:01 -0500

    initial project

---
+++ cdi-ticket/src/main/java/cdi/compute/services/cdi_ticket/App.java
@@ -0,0 +1,13 @@
+package cdi.compute.services.cdi_ticket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
